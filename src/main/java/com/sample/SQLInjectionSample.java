0a8144a34c84cefa98718881ac45570310692ec0
Ramesh Mahimalur <rmahimalur@nimbusconsulting.com>
2021-06-24 12:28:50 -0400

    testing codeql cli

--- src/main/java/com/sample/SQLInjectionSample.java
+++ src/main/java/com/sample/SQLInjectionSample.java
@@ -7,13 +7,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Statement;
 
 public class SQLInjectionSample {
 
     DataSource dataSource;
     private String url = "jdbc:mysql://localhost/test";
-    private String username = "root";
-    private String password = "root1234";
+    private String username = "";
+    private String password = "";
 
 
     private Connection getConn() throws SQLException {
@@ -30,11 +31,20 @@ public class SQLInjectionSample {
                 + customerId
                 + "'";
         Connection c = getConn();
-        ResultSet rs = c.createStatement().executeQuery(sql);
+
+        //////####Vulnerable Code#####//////
+        // ResultSet rs = c.createStatement().executeQuery(sql);
+        // while(rs.next()) {
+        //     accountDTOS.add(new AccountDTO());
+        // }
+
+        //////####Fixed Code#####//////
+        try (Statement stmt = c.createStatement()) {
+          ResultSet rs = stmt.executeQuery(sql);
         while(rs.next()) {
             accountDTOS.add(new AccountDTO());
+          }
         }
         return accountDTOS;
     }
-
 }
